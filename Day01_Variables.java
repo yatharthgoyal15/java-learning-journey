c8f5e720977012c9739ad836e3f0022f18e5abdb
Yatharth <yatharthgoyal15@gmail.com>
2026-06-08 23:45:25 +0530

    Day 1: Variables and Data Types

---
+++ Day01_Variables.java
@@ -0,0 +1,16 @@
+public class Day01_Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Lets learn java");
+         int age = 19;
+        double cgpa = 8.5;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Yatharth";
+
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
+        
+    

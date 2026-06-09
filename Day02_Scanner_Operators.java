81882854880e4b684ebf6fa6c124de650e92162f
Yatharth <yatharthgoyal15@gmail.com>
2026-06-09 23:25:14 +0530

    Update Day 2 Scanner program

---
+++ Day02_Scanner_Operators.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Day02_Scanner_Operators {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Remainder = " + (a % b));
+
+        sc.close();
+    }
+}

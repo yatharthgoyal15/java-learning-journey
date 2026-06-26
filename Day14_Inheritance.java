93756dce538774d70a0d5e9a36d09fa90a03fb4e
Yatharth <yatharthgoyal15@gmail.com>
2026-06-26 23:38:36 +0530

    Day 14: Inheritance in Java

---
+++ Day14_Inheritance.java
@@ -0,0 +1,24 @@
+class Animal {
+
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class Day14_Inheritance {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.eat();
+        d.bark();
+    }
+}
